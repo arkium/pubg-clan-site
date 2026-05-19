1a05d3119f8f0a165b71628ab278886a2592e84d
Arkium SCS <paulo.ferreira@arkium.eu>
2026-05-19 22:04:25 +0200

    Configure Next.js output to standalone
    Add output configuration for standalone mode

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['smk.arkium.group', 'localhost', '127.0.0.1', '10.1.0.248'],
+  output: 'standalone' // <--- Ajoutez cette ligne
+  //allowedDevOrigins: ['smk.arkium.group', 'localhost', '127.0.0.1', '10.1.0.248'],
 };
 
 export default nextConfig;
